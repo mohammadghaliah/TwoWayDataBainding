034b0f6dd985258c2a10de81ab1caca8a4bbbb41
mohammadghaliah <m.ghaliah98@gmail.com>
2022-08-14 17:44:05 +0300

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  value: string;
+
+  clearValue() {
+    this.value = '';
+  }
+
+  nameChanged(arg) {
+    console.log('modelchanged ' + arg);
+  }
+
+  name1Changed(arg) {
+    console.log('name1Changed ' + arg.target.value);
+    console.log(arg);
+  }
 }
